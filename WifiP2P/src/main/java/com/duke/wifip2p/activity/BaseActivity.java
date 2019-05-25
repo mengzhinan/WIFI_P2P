a69a6103ba40ed6d79f0ccc90bac420485a8f8a3
duke <136239173@qq.com>
2019-05-25 18:52:46 +0800

    d

--- WifiP2P/src/main/java/com/duke/wifip2p/activity/BaseActivity.java
+++ WifiP2P/src/main/java/com/duke/wifip2p/activity/BaseActivity.java
@@ -1,6 +1,9 @@
 package com.duke.wifip2p.activity;
 
 import android.Manifest;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 
 import com.duke.dpermission.DPermission;
@@ -23,6 +26,15 @@ public abstract class BaseActivity extends AppCompatActivity {
             Manifest.permission.ACCESS_NETWORK_STATE,
             Manifest.permission.ACCESS_FINE_LOCATION,
             Manifest.permission.WRITE_EXTERNAL_STORAGE};
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.hide();
+        }
+    }
 
     @Override
     protected void onResume() {
